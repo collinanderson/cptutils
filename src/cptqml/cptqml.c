186593bc76537770a4fe3bdd9bfb7c8a441769a7
J.J. Green <j.j.green@gmx.co.uk>
2015-10-25 14:04:57 +0000

    stub for cptqml executable

---
+++ src/cptqml/cptqml.c
@@ -0,0 +1,12 @@
+/*
+  cptqml.c
+  convert GMT colour table (cpt) to QGis style sheet (qml)
+  Copyright (c) J.J. Green 2015
+*/
+
+#include "cptqml.h"
+
+extern int cptqml(cptqml_opt_t opt)
+{
+  return 0;
+}
---
+++ src/cptqml/main.c
@@ -0,0 +1,118 @@
+/*
+  main.c
+
+  part of the gimpcpt package
+
+  This program is free software; you can redistribute it
+  and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURIGHTE.  See the GNU General Public License
+  for more details.
+
+  You should have received a copy of the GNU General Public
+  License along with this program; if not, write to the
+  Free Software Foundation, Inc.,  51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301 USA
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <btrace.h>
+
+#include "options.h"
+#include "cptqml.h"
+
+int main(int argc, char** argv)
+{
+  struct gengetopt_args_info info;
+  cptqml_opt_t opt = {0};
+  char *infile, *outfile;
+
+  /* use gengetopt */
+
+  if (options(argc, argv, &info) != 0)
+    {
+      fprintf(stderr, "failed to parse command line\n");
+      return EXIT_FAILURE;
+    }
+
+  /* check arguments & transfer to opt structure */
+
+  opt.verbose = info.verbose_given;
+
+  /* null outfile for stdout */
+
+  outfile = (info.output_given ? info.output_arg : NULL);
+
+  if (!outfile && opt.verbose)
+    {
+      fprintf(stderr, "verbosity suppressed (<stdout> for results)\n");
+      opt.verbose = 0;
+    }
+
+  /* null infile for stdin */
+
+  switch (info.inputs_num)
+    {
+    case 0:
+      infile = NULL;
+      break;
+    case 1:
+      infile = info.inputs[0];
+      break;
+    default:
+      fprintf(stderr, "Sorry, only one file at a time\n");
+      return EXIT_FAILURE;
+    }
+
+  if (opt.verbose)
+    printf("This is cptqml (version %s)\n", VERSION);
+
+  opt.file.input = infile;
+  opt.file.output = outfile;
+
+  btrace_enable("cptqml");
+
+  int err = cptqml(opt);
+
+  if (err)
+    {
+      btrace("failed (error %i)", err);
+      btrace_print_stream(stderr, BTRACE_PLAIN);
+
+      if (info.backtrace_file_given)
+	{
+	  int format = BTRACE_XML;
+	  if (info.backtrace_format_given)
+	    {
+	      format = btrace_format(info.backtrace_format_arg);
+	      if (format == BTRACE_ERROR)
+		{
+		  fprintf(stderr, "no such backtrace format %s\n",
+			  info.backtrace_format_arg);
+		  return EXIT_FAILURE;
+		}
+	    }
+	  btrace_print(info.backtrace_file_arg, format);
+	}
+    }
+
+  btrace_reset();
+  btrace_disable();
+
+  if (opt.verbose)
+    printf("done.\n");
+
+  options_free(&info);
+
+  return (err ? EXIT_FAILURE : EXIT_SUCCESS);
+}
