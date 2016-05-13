f65a29bd406d0ae98c8994c138d28b655a2bfff5
J.J. Green <j.j.green@gmx.co.uk>
2016-05-13 23:12:33 +0100

    ucs2trf8 memory leak (coverity)

--- src/pssvg/ucs2utf8.c
+++ src/pssvg/ucs2utf8.c
@@ -15,27 +15,35 @@ extern int ucs2_to_utf8(const char *ucs2,
   /* UCS-2BE seems to work, so does UTF-16BE */
 
   iconv_t cd = iconv_open("UTF-8", "UCS-2BE");
-  if (cd == (iconv_t)(-1)) return 1;
 
-  size_t 
-    ucs2_bytes_left = ucs2len, 
-    utf8_bytes_left = utf8len,
-    converted;
-
-  converted = iconv(cd, (char**)&(ucs2), &(ucs2_bytes_left),
-		    &(utf8), &(utf8_bytes_left));
-
-  if (converted == (size_t)-1)
+  if (cd == (iconv_t)(-1))
     {
       btrace("error in iconv: %s\n", strerror(errno));
       return 1;
     }
 
+  size_t
+    ucs2_bytes_left = ucs2len,
+    utf8_bytes_left = utf8len,
+    converted;
+
+  converted =
+    iconv(cd, (char**)&(ucs2),
+	  &(ucs2_bytes_left), &(utf8), &(utf8_bytes_left));
+
+  int err = 0;
+
+  if (converted == (size_t)-1)
+    {
+      btrace("error in iconv: %s\n", strerror(errno));
+      err++;
+    }
+
   if (iconv_close(cd) == -1)
     {
       btrace("error closing iconv descriptor: %s\n", strerror(errno));
-      return 1;
+      err++;
     }
 
-  return 0;
+  return err > 0;
 }
